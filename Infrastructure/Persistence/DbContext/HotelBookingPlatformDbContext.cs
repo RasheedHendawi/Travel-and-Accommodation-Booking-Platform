d8708979367ff6f56b59e18c023aa5a9efcdb108
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-17 18:14:23 +0200

    Adding BookingPlatformDbContext

---
+++ Infrastructure/Persistence/DbContext/HotelBookingPlatformDbContext.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Entities;
+using Infrastructure.Persistence.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure.Persistence.DbContext
+{
+    public class HotelBookingPlatformDbContext(DbContextOptions<HotelBookingPlatformDbContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Discount> Discounts { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomClass> RoomClasses { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+        public DbSet<Amenity> Amenities { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfig());
+            modelBuilder.ApplyConfiguration(new RoleConfig());
+            modelBuilder.ApplyConfiguration(new DiscountConfig());
+            modelBuilder.ApplyConfiguration(new ReviewConfig());
+            modelBuilder.ApplyConfiguration(new RoomConfig());
+            modelBuilder.ApplyConfiguration(new RoomClassConfig());
+            modelBuilder.ApplyConfiguration(new HotelConfig());
+            modelBuilder.ApplyConfiguration(new BookingConfig());
+            modelBuilder.ApplyConfiguration(new AmenityConfig());
+            modelBuilder.ApplyConfiguration(new CityConfig());
+            modelBuilder.ApplyConfiguration(new OwnerConfig());
+            modelBuilder.ApplyConfiguration(new ImageConfig());
+        }
+
+    }
+}
