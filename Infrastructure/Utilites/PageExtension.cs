f110497dd88dc19055218c02ab6c6235f30f7cec
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-20 22:55:50 +0200

    Add 'SortingUtilites'  files

---
+++ Infrastructure/Utilites/PageExtension.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure.Utilites
+{
+    public static class PageExension
+    {
+        public static IQueryable<TItem> GetPage<TItem>(
+          this IQueryable<TItem> queryable,
+          int pageNumber, int pageSize)
+        {
+            return queryable.Skip(pageSize * (pageNumber - 1))
+              .Take(pageSize);
+        }
+
+        public static async Task<PaginationMetaData> GetPaginationDataAsync<TItem>(
+          this IQueryable<TItem> queryable,
+          int pageNumber, int pageSize)
+        {
+            return new PaginationMetaData(
+              await queryable.CountAsync(),
+              pageNumber,
+              pageSize);
+        }
+    }
+}
---
+++ Infrastructure/Utilites/SortingExtision.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Enums;
+using System.ComponentModel;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Utilites
+{
+    public static class SortingExtision
+    {
+        public static IQueryable<TItem> Sort<TItem>(
+          this IQueryable<TItem> queryable,
+          Expression<Func<TItem, object>> sortColumnExpression,
+          SortMethod sortOrder)
+        {
+            return sortOrder switch
+            {
+                SortMethod.Ascending => queryable.OrderBy(sortColumnExpression),
+                SortMethod.Descending => queryable.OrderByDescending(sortColumnExpression),
+                _ => throw new InvalidEnumArgumentException()
+            };
+        }
+    }
+}
