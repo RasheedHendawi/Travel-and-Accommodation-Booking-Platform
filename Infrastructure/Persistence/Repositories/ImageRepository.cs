a7ffd16e3ba4603e0842c93d39920a680332f824
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-24 23:32:12 +0200

    Add `ImageRepository` Implementation TODO ImageService to complete this repository.

--- Infrastructure/Persistence/Repositories/ImageRepository.cs
+++ Infrastructure/Persistence/Repositories/ImageRepository.cs
@@ -1,12 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Entities;
+using Domain.Enums;
+using Domain.Interfaces.Repositories;
+using Infrastructure.Persistence.ContextDb;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence.Repositories
 {
-    internal class ImageRepository
+    public class ImageRepository(HotelBookingPlatformDbContext context) : IImageRepository
     {
+        public async Task<Image> CreateAsync(IFormFile image, Guid id, ImageType type)
+        {
+            //TODO: Implement ImageRepository.CreateAsync
+            throw new System.NotImplementedException();
+        }
+
+        public async Task DeleteAsync(Guid id, ImageType type)
+        {
+            var image = await context.Images.Where(i => i.EntityId == id && i.Type ==type)
+                .ToListAsync();
+            foreach (var img in image)
+            {
+                context.Images.Remove(img);
+            }
+        }
     }
 }
