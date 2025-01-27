e0d9de02ca11ad53bc66a59da4f742cbaa86931c
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-27 19:41:51 +0200

    Update `Roleconfig.cs` to seed the data

--- Infrastructure/Persistence/Configuration/RoleConfig.cs
+++ Infrastructure/Persistence/Configuration/RoleConfig.cs
@@ -12,6 +12,12 @@ namespace Infrastructure.Persistence.Configuration
             builder.HasMany(r => r.Users)
                 .WithMany(u => u.Roles);
 
+            builder.HasData
+                ([
+                    new Role {Id = new Guid("bd0cb72e-df0a-4514-99d8-52bb81b41340"), Name = "Guest" },
+                    new Role {Id = new Guid("c23401af-cbb8-4b73-8d6f-ecbb5e31d3b7"), Name = "Admin" }
+                ]);
+
         }
     }
 }
