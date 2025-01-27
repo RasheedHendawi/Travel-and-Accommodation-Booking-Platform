cd08935bf9cd26f169a9986a883820510e240812
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-27 19:58:58 +0200

    Create `Mapping` file

---
+++ Application/Mapping/UsersProfile.cs
@@ -0,0 +1,16 @@
+﻿using Application.Users;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Models;
+
+namespace Application.Mapping
+{
+    public class UsersProfile : Profile
+    {
+        public UsersProfile()
+        {
+            CreateMap<RegisterHandler, User>();
+            CreateMap<JwtToken, LoginResult>();
+        }
+    }
+}
