8f84064d1f01a5d197792737b04ad733cc2f66c3
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-27 20:00:03 +0200

    Create `ApplicationConfiguration.cs` for the DI in the applicaiton layer

---
+++ Application/ApplicaitonConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using Application.Contracts;
+using Application.Users;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application
+{
+    public static class ApplicaitonConfiguration
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            RegisterServices(services);
+            return services;
+        }
+
+        private static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+        }
+    }
+}
