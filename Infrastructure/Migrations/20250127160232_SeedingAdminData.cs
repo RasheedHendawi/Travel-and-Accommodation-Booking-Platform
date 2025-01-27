75aaa2e920a9cc02b939b7a31060e2b1ffda5270
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-27 19:41:24 +0200

    Seed `AdminData` for the Admin Deatils in the UserRoles table also

---
+++ Infrastructure/Migrations/20250127160232_SeedingAdminData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingAdminData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "FirstName", "LastName", "Password" },
+                values: new object[] { new Guid("f403d2ed-7f0d-499c-a6e9-7e3d751f842a"), "Admin@hotelManager.com", "Super", "Admin", "AQAAAAIAAYagAAAAEMcAhymzoRbYY1s8WP2AWcrQV3CHk35ny+1XHcuYxyfVqKIy5IaRVHzHa4SqBJOzFQ==" });
+
+            migrationBuilder.InsertData(
+                table: "UserRole",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { new Guid("c23401af-cbb8-4b73-8d6f-ecbb5e31d3b7"), new Guid("f403d2ed-7f0d-499c-a6e9-7e3d751f842a") });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "UserRole",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { new Guid("c23401af-cbb8-4b73-8d6f-ecbb5e31d3b7"), new Guid("f403d2ed-7f0d-499c-a6e9-7e3d751f842a") });
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("f403d2ed-7f0d-499c-a6e9-7e3d751f842a"));
+        }
+    }
+}
