efe1075ecebb97f95e0d11588a3505476a8e36f2
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-27 19:42:33 +0200

    Update `Userconfig.cs` to seed the data from it

--- Infrastructure/Persistence/Configuration/UserConfig.cs
+++ Infrastructure/Persistence/Configuration/UserConfig.cs
@@ -19,8 +19,29 @@ namespace Infrastructure.Persistence.Configuration
                 .OnDelete(DeleteBehavior.Cascade),
                 a => a.HasOne<User>().WithMany().HasForeignKey("UserId")
                 .OnDelete(DeleteBehavior.Cascade));
-
-
+            builder.HasData
+                ([
+                    new User
+                    {
+                        Id = new Guid("f403d2ed-7f0d-499c-a6e9-7e3d751f842a"),
+                        FirstName = "Super",
+                        LastName = "Admin",
+                        Email = "Admin@hotelManager.com",
+                        Password= "AQAAAAIAAYagAAAAEMcAhymzoRbYY1s8WP2AWcrQV3CHk35ny+1XHcuYxyfVqKIy5IaRVHzHa4SqBJOzFQ=="
+                    }
+                ]);
+            builder.HasMany(u => u.Roles)
+              .WithMany(r => r.Users)
+              .UsingEntity<Dictionary<string, object>>(
+                "UserRole",
+                j => j.HasOne<Role>().WithMany()
+                  .HasForeignKey("RoleId").OnDelete(DeleteBehavior.Cascade),
+                j => j.HasOne<User>().WithMany()
+                  .HasForeignKey("UserId").OnDelete(DeleteBehavior.Cascade))
+                  .HasData([new Dictionary<string, object>{
+                        ["UserId"] = new Guid("f403d2ed-7f0d-499c-a6e9-7e3d751f842a"),
+                        ["RoleId"] = new Guid("c23401af-cbb8-4b73-8d6f-ecbb5e31d3b7")
+                    }]);
 
 
             builder.HasIndex(u => u.Email).IsUnique();
