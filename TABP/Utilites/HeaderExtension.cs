12cd455999d9b4a388c110651206d22fca869d31
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-28 21:26:56 +0200

    Add `Utilites` fiile

---
+++ TABP/Utilites/HeaderExtension.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Models;
+using System.Text.Json;
+namespace TABP.Utilites
+{
+    public static class HeaderExtension
+    {
+        public static void AddPaginationData(this IHeaderDictionary headers,
+          PaginationMetaData paginationMetadata)
+        {
+            headers["x-pagination"] = JsonSerializer.Serialize(paginationMetadata);
+        }
+    }
+}
