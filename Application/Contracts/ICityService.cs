b866f713de9fe2c2de7baade6cb974c884699371
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-28 21:21:22 +0200

    Add `ICityService.cs`

---
+++ Application/Contracts/ICityService.cs
@@ -0,0 +1,17 @@
+﻿using Application.Cities.Models;
+using Application.DTOs.Cities;
+using Application.DTOs.Images;
+using Domain.Models;
+
+namespace Application.Contracts
+{
+    public interface ICityService
+    {
+        Task<PaginatedList<CityForManagementResponse>> GetCitiesForManagementAsync(CitiesGetHandler request);
+        Task<IEnumerable<TrendingCityResponse>> GetTrendingCitiesAsync(int count);
+        Task CreateCityAsync(CityCreationRequest request);
+        Task UpdateCityAsync(Guid id, CityUpdateRequest request);
+        Task DeleteCityAsync(Guid id);
+        Task SetCityThumbnailAsync(Guid id, ImageCreationRequest request);
+    }
+}
