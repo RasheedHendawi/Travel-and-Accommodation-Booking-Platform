d8c093f4e288aad6cd1e3d1e5bf564f11ab5f1f8
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-30 04:32:12 +0200

    Add `Owner/Dtos`

---
+++ Application/DTOs/Owners/OwnersGetRequest.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Enums;
+
+namespace Application.DTOs.Owners;
+
+public class OwnersGetRequest
+{
+    public string? SearchTerm { get; set; }
+    public string? SortColumn { get; set; }
+    public SortMethod? SortOrder { get; set; }
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+}
