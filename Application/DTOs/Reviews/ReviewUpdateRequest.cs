036d9502bfac4556bfae3ec334fb6e06708b1d04
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-30 04:32:25 +0200

    Add `Review/Dtos`

---
+++ Application/DTOs/Reviews/ReviewUpdateRequest.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Application.DTOs.Reviews
+{
+    public class ReviewUpdateRequest
+    {
+        public string Content { get; init; }
+        public int Rating { get; init; }
+    }
+}
