83be6fe02735f210e6e9c5ea6016995385bf0ed2
Rasheed Hendawi <rrash22875@gmail.com>
2025-01-30 04:33:37 +0200

    Add `ProfileConfiguration` for the City/Hotel/Owner/Review

---
+++ Application/Mapping/HotelsProfile.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs.Hotels;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Models;
+
+namespace Application.Mapping
+{
+    public class HotelsProfile : Profile
+    {
+        public HotelsProfile()
+        {
+            CreateMap<HotelCreationRequest, Hotel>();
+            CreateMap<HotelUpdateRequest, Hotel>();
+            CreateMap<Hotel, HotelGetFromManagment>();
+            CreateMap<Hotel, HotelGetResponse>();
+            CreateMap<HotelForManagement, HotelGetFromManagment>();
+            CreateMap<Hotel, HotelSearchResponse>();
+            CreateMap<RoomClass, HotelFeaturedDealResponse>();
+        }
+    }
+}
---
+++ Application/Mapping/OwnersProfile.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs.Owners;
+using AutoMapper;
+using Domain.Entities;
+using Domain.Models;
+
+namespace Application.Mapping
+{
+    public class OwnersProfile : Profile
+    {
+        public OwnersProfile()
+        {
+            CreateMap<OwnerCreationRequest, Owner>();
+            CreateMap<OwnerUpdateRequest, Owner>();
+            CreateMap<Owner, OwnersResponse>();
+            CreateMap<PaginatedList<Owner>, PaginatedList<OwnersResponse>>();
+        }
+    }
+}
